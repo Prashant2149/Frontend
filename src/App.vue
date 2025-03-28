049a063422199231f034fae75341f88d73ee992b
Prashant2149 <singhprasooo@gmail.com>
2025-03-29 01:35:50 +0530

    Fiesr commit in frontend

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.firstName }} {{ user.lastName }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:3000/users');
+      this.users = response.data;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  padding: 5px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
